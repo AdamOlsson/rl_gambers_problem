904635ba2a47fc790431b80cb1f38ffb6bd1b48f
AdamOlsson <Olsson.95_@hotmail.com>
2019-08-19 12:14:08 +0200

    value iteration

---
+++ value_iteration.py
@@ -0,0 +1,9 @@
+import numpy as np
+from gamblers_problem import GP
+
+def value_iteration(env, discount_factor=1.0, epsilon=0.0001):
+    return 0,0
+
+if __name__ == "__main__":
+    env = GP()
+    p, v = value_iteration(env)
